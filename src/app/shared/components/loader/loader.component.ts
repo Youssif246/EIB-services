ad447b223b6db0d4b3b7afdec179b0b22de49a71
youssif1 <financialexpert2020@gmail.com>
2026-09-13 21:22:38 +0300

    Add loader

---
+++ src/app/shared/components/loader/loader.component.ts
@@ -0,0 +1,28 @@
+import { Component, PLATFORM_ID, inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
+@Component({
+  selector: 'app-loader',
+  standalone: true,
+  templateUrl: './loader.component.html',
+  styleUrl: './loader.component.css'
+})
+export class LoaderComponent {
+  private platformId = inject(PLATFORM_ID);
+  isFinished = false;
+
+  constructor() {
+    if (isPlatformBrowser(this.platformId)) {
+      // Safety timeout to guarantee DOM cleanup after curtain lift finishes
+      setTimeout(() => {
+        this.isFinished = true;
+      }, 3500);
+    }
+  }
+
+  onAnimationEnd(event: AnimationEvent) {
+    if (event.animationName === 'eibCurtainLift' || event.animationName === 'eibQuickDismiss') {
+      this.isFinished = true;
+    }
+  }
+}
